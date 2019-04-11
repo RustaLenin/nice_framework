63587ef834ec4295ed3dfedd4ae0d98f12befcbb
Artem Anik <acas97@mail.ru>
2019-04-11 13:52:00 +0300

    theme tourist

--- components/vars/nice_vars.js
+++ components/vars/nice_vars.js
@@ -75,15 +75,15 @@ export function notFoundTemplate() {
             /** BackGrounds **/
             --main_background: #F7F3F2;
             --content_background: #ffffff;
-            --accent_background: #2aa3e6;
-            --second_accent_background: #FF6600;
+            --accent_background: #58c5c7;
+            --second_accent_background: #9d56a2;
             --sub_bacground: #EAEAEA;
             --accent_sub_background: #BCBCBC;
         
             /** Borders and Dividers **/
             --divider_color: #D1CFCF;
-            --accent_border_color: #2aa3e6;
-            --accent_hover_border_color: #FF6600;
+            --accent_border_color: #58c5c7;
+            --accent_hover_border_color: #9d56a2;
         
             /** Fields **/
             --field_border: #C4C4C4;
@@ -95,8 +95,8 @@ export function notFoundTemplate() {
             --field_validate_success: #1AC119;
         
             /** Buttons **/
-            --accent_button_color: #2aa3e6;
-            --accent_second_button_color: #FF6600;
+            --accent_button_color: #58c5c7;
+            --accent_second_button_color: #9d56a2;
             --button_submit_color: #ffffff;
             --disabled_button_color: #888888;
             --bad_button: #E00000;
@@ -108,20 +108,20 @@ export function notFoundTemplate() {
             --error: #FB6875;
         
             /** Font **/
-            --regular_font_family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-            --regular_font_color: #212121;
+            --regular_font_family:'manrope', sans-serif;
+             --regular_font_color: #212121;
             --second_font_color: #4D4D4D;
-            --accent_font_color: #2aa3e6;
+            --accent_font_color: #58c5c7;
             --sub_font_color: #B1B1B1;
             --support_font_color: #8C8C8C;
-            --accent_hover_font_color: #FF6600;
+            --accent_hover_font_color: #9d56a2;
             --font_reverce_accent_color: #ffffff;
         
             /** Icons **/
             --unactive_icon_color: #D1CFCF;
             --icon_color: #9E9E9E;
-            --icon_hover_color: #3784DC;
-            --accent_icon_color: #2aa3e6;
+            --icon_hover_color: #58c5c7;
+            --accent_icon_color: #9d56a2;
             --accent_hover_icon_color: #FF6600;
             --icon_reverce_accent_color: #ffffff;
         
