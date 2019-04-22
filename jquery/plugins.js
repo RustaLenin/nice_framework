c06dde1e90f120ebe4e1a4bb45b25e943fd6178b
RLenin <RustaLenin@mail.ru>
2019-04-22 15:11:42 +0300

    Remove isEqual() - not working

--- jquery/plugins.js
+++ jquery/plugins.js
@@ -11,37 +11,3 @@ export function update_jQuery() {
         });
     };
 }
-
-
-
-(function() {
-
-    window.isEqual = function(a, b) {
-        var p, t;
-        for (p in a) {
-            if (typeof b[p] === 'undefined') {
-                return false;
-            }
-            if (b[p] && !a[p]) {
-                return false;
-            }
-            t = typeof a[p];
-            if (t === 'object' && !isEqual(a[p], b[p])) {
-                return false;
-            }
-            if (t === 'function' && (typeof b[p] === 'undefined' || a[p].toString() !== b[p].toString())) {
-                return false;
-            }
-            if (a[p] !== b[p]) {
-                return false;
-            }
-        }
-        for (p in b) {
-            if (typeof a[p] === 'undefined') {
-                return false;
-            }
-        }
-        return true;
-    };
-
-}).call(this);
