88a7e193e3b4c203a3f6d8b91cab9965a33d38d7
RLenin <RustaLenin@mail.ru>
2019-07-12 19:10:59 +0300

    Start working on settings component

--- components/settings/settings.js
+++ components/settings/settings.js
@@ -1,3 +1,72 @@
+export class NiceSettings extends HTMLElement {
+
+    defaultModel = {
+        'title': 'Nice Settings',
+    };
+
+    currentModel = {};
+
+    constructor() {
+        super();
+        this.innerHTML = this.render();
+    }
+
+    render() {
+
+        let buffer = ``;
+        buffer += this.renderHeader();
+        buffer += this.renderMenu();
+        buffer += this.renderContent();
+
+        return buffer;
+
+    }
+
+    renderHeader() {
+        let model = this.currentModel;
+        return `
+    <div class="settings_header">
+
+        <div class="settings_title">
+            ${ model.title }
+        </div>
+
+        <div class="settings_buttons">
+
+            <span class="nice_button small" onclick="Nice.settings.expandAllTabsBlocks(this)">
+                ${Nice.svg({'id': 'double_arrow_down', 'size': 'small'})} 
+                ${_t('Expand All')}
+            </span>
+            
+            <span class="nice_button small" onclick="Nice.settings.collapseAllTabsBlocks(this)">
+                ${Nice.svg({'id': 'double_arrow_down', 'size': 'small', 'rotate': true })} 
+                ${_t('Collapse All')}
+            </span>
+
+            <span class="nice_button small submit" onclick="Nice.settings.updateSettings(this)">
+                ${Nice.svg({'id': 'double_arrow_down', 'size': 'small', 'rotate': true })} 
+                ${_t('Save Settings')}
+            </span>
+
+        </div>
+
+    </div>`;
+    }
+
+    renderMenu() {
+        let model = this.currentModel;
+        return ``;
+    }
+
+    renderContent() {
+        let model = this.currentModel;
+        return ``;
+    }
+
+}
+
+
+
 export function toggleCollapseSettingsMenu( elem ) {
     jQuery(elem).parent('.SettingsNavigation').toggleClass('collapsed');
 }
@@ -34,6 +103,4 @@ export function updateSettings( elem ) {
     let settingsBlock = jQuery(elem).parents('.SettingsContainer');
     let data = Nice.form.collectData( '.input', settingsBlock);
     console.log( data );
-
-    /** ToDo Complete saving data  **/
 }
