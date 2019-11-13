b6b3b15798d9179f17b73a22f4478078764259aa
RLenin <RustaLenin@mail.ru>
2019-11-13 14:26:03 +0300

    Star svg

--- components/svg/map.js
+++ components/svg/map.js
@@ -1090,6 +1090,15 @@ const stats = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="31
         C11.389,21.674,10.921,21.225,10.344,21.225z"/>
 </svg>`;
 
+const star = `<svg  xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 940.688 940.688" style="enable-background:new 0 0 940.688 940.688;"
+	 xml:space="preserve">
+<g>
+	<path d="M885.344,319.071l-258-3.8l-102.7-264.399c-19.8-48.801-88.899-48.801-108.6,0l-102.7,264.399l-258,3.8
+		c-53.4,3.101-75.1,70.2-33.7,103.9l209.2,181.4l-71.3,247.7c-14,50.899,41.1,92.899,86.5,65.899l224.3-122.7l224.3,122.601
+		c45.4,27,100.5-15,86.5-65.9l-71.3-247.7l209.2-181.399C960.443,389.172,938.744,322.071,885.344,319.071z"/>
+</g>
+</svg>`;
+
 const top = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
 	  viewBox="0 0 566.848 566.848" style="enable-background:new 0 0 566.848 566.848;"
 	 xml:space="preserve">
@@ -1375,6 +1384,7 @@ export const SvgMap = {
     'review': review,
     'ruble': ruble,
     'remove_img': remove_img,
+
     /** S **/
     'search': search,
     'sign_in': sign_in,
@@ -1384,6 +1394,7 @@ export const SvgMap = {
     'success-icon': success_icon,
     'send': send,
     'stats': stats,
+    'star': star,
 
     /** T **/
     'top': top,
